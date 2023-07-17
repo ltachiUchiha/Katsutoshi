e8f39b78099266332c243ee5da768e2c4458e70c
ltachiUchiha <itachiuchihadev@gmail.com>
2023-07-17 23:10:48 +0300

    Removed and sorted usings

--- Katsutoshi/ViewModels/DrivesViewModel.cs
+++ Katsutoshi/ViewModels/DrivesViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using Katsutoshi.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Katsutoshi.ViewModels
 {
